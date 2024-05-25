04f1aeb1fcff16f2c5d9b7a5ad6f7df1f7fdc478
TLiLi HAMDI <tlilios@yahoo.com>
2024-05-25 02:23:15 +0200

    OnGoing

--- UIViewAnimation/UIViewAnimation/ViewController.swift
+++ UIViewAnimation/UIViewAnimation/ViewController.swift
@@ -8,12 +8,45 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var btnFirst: UIButton!
+    @IBOutlet weak var btnSecond: UIButton!
+    @IBOutlet weak var btnThird: UIButton!
+    @IBOutlet weak var btnFourth: UIButton!
+    @IBOutlet weak var imgHeart: UIImageView!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        btnFirst.alpha = 0
+        btnSecond.alpha = 0
+        btnThird.alpha = 0
+        btnFourth.alpha = 0
+        
     }
 
+    override func viewDidAppear(_ animated: Bool) {
+        animateItems()
+    }
+    
+    func animateItems() {
+        UIView.animate(withDuration: 1.5) {
+            self.btnFirst.alpha = 1
+            self.btnSecond.alpha = 1
+            self.btnThird.alpha = 1
+            self.btnFourth.alpha = 1
+        }
+        UIView.animate(withDuration: 1.5) {
+            self.btnFirst.center.y += 50
+            self.btnSecond.center.y += 50
+            self.btnThird.center.y += 50
+            self.btnFourth.center.y += 50
+        } completion: { _ in
+            
+        }
+
+    }
+
+    
 
 }
 
